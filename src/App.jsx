205005304da80c041b5bf26e2fa463f9d2c82a5b
ivantorale <ivantorale@campus.monlau.com>
2023-03-22 20:01:46 +0100

    M9_ReactBootstrap_Torres

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Button } from 'react-bootstrap';
+
+function App() {
+  return (
+    <div>
+    <h1>ONG Ivan</h1>
+    <p>Información sobre la ONG...</p>
+    <Button variant="primary">Donar</Button>
+    </div>
+    );
+    
+}
+
+
+export default App
